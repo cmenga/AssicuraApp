6f4a81aadd857ecb1c053b97bb0cf74cabc0c485
Cosimo Menga <cosimo_menga@ik.me>
2026-01-14 13:54:00 +0100

    Add axios and identity API client

---
+++ frontend/src/api/identity.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const authApi = axios.create({
+  baseURL: "http://localhost:8001/auth",
+  validateStatus: () => true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
