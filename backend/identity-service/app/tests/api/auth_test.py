050cfea642752be1223d0711b6c10eec575f9dd2
Cosimo Menga <cosimo_menga@ik.me>
2026-01-15 20:14:11 +0100

    Add sign-in tests and remove province_of_birth

--- backend/identity-service/app/tests/api/auth_test.py
+++ backend/identity-service/app/tests/api/auth_test.py
@@ -12,7 +12,6 @@ VALID_USER = {
     "last_name": "Rossi",
     "date_of_birth": "1990-01-01",
     "place_of_birth": "Milano",
-    "province_of_birth": "Milano",
     "gender": "male",
     "phone_number": "3331234567",
     "password": "Password1!",
@@ -37,6 +36,12 @@ def test_signup_success(client):
     """
     The function `test_signup_success` sends a POST request to the "/auth/sign-up" endpoint with valid
     user and address data and asserts that the response status code is 204.
+    
+    Args:
+        client: The `client` parameter in the `test_signup_success` function is typically an instance of a
+            test client that is used to make requests to your API endpoints during testing. This client allows
+            you to simulate HTTP requests and test the responses from your API without actually making real
+            network requests. It is commonly used
     """
     response = client.post(
         "/auth/sign-up",
@@ -50,7 +55,13 @@ def test_signup_success(client):
 
 def test_signup_user_already_exists(client):
     """
-    The function tests signing up a user who already exists in the system.
+    The function `test_signup_user_already_exists` tests signing up a user who already exists.
+    
+    Args:
+        client: The `client` parameter in the `test_signup_user_already_exists` function is likely an
+            instance of a test client that is used to make HTTP requests to the application being tested. In
+            this case, it is being used to simulate a POST request to the "/auth/sign-up" endpoint with JSON
+            data
     """
     response = client.post(
         "/auth/sign-up",
@@ -64,6 +75,12 @@ def test_signup_user_already_exists(client):
 def test_signup_password_mismatch(client):
     """
     The function `test_signup_password_mismatch` tests signing up with mismatched passwords.
+    
+    Args:
+        client: The `client` parameter in the `test_signup_password_mismatch` function is likely an
+            instance of a test client that is used to make HTTP requests to your application during testing. It
+            is commonly used in testing frameworks like Flask's test client or Django's test client to simulate
+            requests to your API endpoints
     """
     bad_user = VALID_USER.copy()
     bad_user["confirm_password"] = "WrongPassword1!"
@@ -74,3 +91,72 @@ def test_signup_password_mismatch(client):
     )
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_sign_in_success(client):
+    """
+    The function `test_sign_in_success` tests the successful sign-in functionality by sending a POST
+    request with email and password, and asserts the expected response status code and content.
+    
+    Args:
+        client: The `client` parameter in the `test_sign_in_success` function is typically an instance of
+            a test client that is used to make HTTP requests to your API endpoints during testing. This client
+            is usually provided by the testing framework you are using, such as Flask's test client or Django's
+            test client
+    """
+    response = client.post(
+        "/auth/sign-in",
+        json={
+            "email": "test@example.com",
+            "password": "Password1!"
+        }
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert "access_token" in data 
+    assert "refresh_token" in data
+    assert "type" in data and data["type"] == "Bearer"
+    
+def test_sign_in_not_success(client):
+    """
+    The function `test_sign_in_not_success` sends a POST request to the "/auth/sign-in" endpoint with
+    specific credentials and asserts that the response status code is 404 Not Found.
+    
+    Args:
+        client: The `client` parameter in the `test_sign_in_not_success` function is likely an instance of
+            a client object that is used for making HTTP requests in a testing environment. It is commonly used
+            in testing frameworks like Flask's test client or Django's test client to simulate HTTP requests to
+            the application being
+    """
+    response = client.post(
+        "/auth/sign-in",
+        json={
+            "email": "test@s.com",
+            "password": "Password1!"
+        }
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+def test_sign_in_unauthorized(client):
+    """
+    The function `test_sign_in_unauthorized` sends a POST request to the "/auth/sign-in" endpoint with a
+    specific email and password, and asserts that the response status code is 401 (Unauthorized).
+    
+    Args:
+        client: The `client` parameter in the `test_sign_in_unauthorized` function is likely an instance
+            of a test client that is used to make HTTP requests to your application during testing. It is
+            commonly used in testing frameworks like Flask's `test_client` or Django's `Client` to simulate
+            requests to
+    """
+    response = client.post(
+        "/auth/sign-in",
+        json={
+            "email": "test@example.com",
+            "password": "Password1"
+        }
+    )
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
