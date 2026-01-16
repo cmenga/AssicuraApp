94fc58a41e74111d9afebfc2285a222917ebcec6
Cosimo Menga <cosimo_menga@ik.me>
2026-01-16 12:13:51 +0100

    Simplify get_logged_user endpoint logic

--- backend/identity-service/app/api/user/router.py
+++ backend/identity-service/app/api/user/router.py
@@ -10,13 +10,9 @@ user_router = APIRouter(tags=["user"], prefix="/user")
 
 
 @user_router.get("/me",status_code=status.HTTP_200_OK)
-async def get_logged_user(auth_token: auth_dependency, db: db_dependency,jwt: jwt_dependency) -> UserWithAddressOut :
+async def get_logged_user(auth_token: auth_dependency, db: db_dependency,jwt: jwt_dependency) -> UserDataOut :
     user = get_current_user(db,jwt.decode_access_token(auth_token)) 
-    addresses = get_addresses(db, str(user.id))
+    return UserDataOut.model_validate(user)
+
     
-    normalized_user = UserDataOut.model_validate(user)
-    normalized_adresses = list()
-    for address in addresses:
-        normalized_adresses.append(AddressDataOut.model_validate(address))
-    
-    return UserWithAddressOut(user=normalized_user, address=normalized_adresses)
+  
