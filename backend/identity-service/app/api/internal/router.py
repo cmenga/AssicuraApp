ca1753a034e99763a6a0a63d733bdd524eb7b579
Cosimo Menga <cosimo_menga@ik.me>
2026-01-29 12:31:40 +0100

    Update driver-license service secret key

--- backend/identity-service/app/api/internal/router.py
+++ backend/identity-service/app/api/internal/router.py
@@ -1,23 +1,27 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 from jose import jwt
 import hmac
 
 
 from datetime import datetime, timedelta, timezone
-from api.internal.schema import ServiceTokenRequest,ServiceTokenResponse
+from api.internal.schema import ServiceTokenRequest, ServiceTokenResponse
 from api.exceptions import HTTPUnauthorized
 from settings import get_secret_key
 
 internal_router = APIRouter(prefix="/internal", include_in_schema=False)
 
 
-SERVICES: dict = {"driver-license": "secret-key"}
+SERVICES: dict = {
+    "driver-license": "-DZMY0hvzifPyi8Lj_GxBkvuxzxkqY3lf0U5Ckcf-pHpUX61iCjePeSWQTQeUAHtLSqqK9j1gSOc4B0wnlYZUA"
+}
 JWT_SECRET = get_secret_key()
+
 
 def create_jwt(payload: dict) -> str:
     payload = payload.copy()
     payload["exp"] = datetime.now(timezone.utc) + timedelta(minutes=30)
     return jwt.encode(payload, JWT_SECRET, algorithm="HS256")
+
 
 @internal_router.post("/service-token", response_model=ServiceTokenResponse)
 def issue_service_token(data: ServiceTokenRequest):
