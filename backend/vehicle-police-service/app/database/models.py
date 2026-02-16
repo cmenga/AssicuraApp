d1d404429e83d66c73dcddaaf31730d8399e2204
Cosimo Menga <cosimo_menga@ik.me>
2026-02-16 11:41:30 +0100

    Add Vehicle model and migration

--- backend/vehicle-police-service/app/database/models.py
+++ backend/vehicle-police-service/app/database/models.py
@@ -1,9 +1,28 @@
-from sqlalchemy import Column, Boolean, UUID
-from uuid import uuid4
+import uuid
+from sqlalchemy import String, UUID, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
 
 from database.session import Base
 
-class TestModel(Base):
-    __tablename__ = "Test"
-    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4)
-    debug_flag = Column(Boolean, default=False)
+class Vehicle(Base):
+    __tablename__ = "vehicles"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+
+    license_plate: Mapped[str] = mapped_column(
+        String(10), unique=True, index=True, nullable=False
+    )
+    vin: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    brand: Mapped[str] = mapped_column(String(50), nullable=False)
+    model: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),nullable=False,
+    )
+
+    __table_args__ = (
+        UniqueConstraint("vin", "user_id", name="uq_vehicle_owner"),
+    )
