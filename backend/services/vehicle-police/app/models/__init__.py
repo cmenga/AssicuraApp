b8d850a970c19609cb14ad5806d150ae66b5e492
Cosimo Menga <cosimo_menga@ik.me>
2026-02-22 19:17:20 +0100

    Add Contract model and migration

--- backend/services/vehicle-police/app/models/__init__.py
+++ backend/services/vehicle-police/app/models/__init__.py
@@ -2,3 +2,4 @@ from core.database import Base
 
 from .vehicle import Vehicle
 from .insurance_policy import InsurancePolicy
+from .contract import Contract
---
+++ backend/services/vehicle-police/app/models/contract.py
@@ -0,0 +1,59 @@
+import uuid
+from core.database import Base
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import UUID
+from sqlalchemy import Float
+from sqlalchemy import Integer
+from sqlalchemy import DateTime
+from sqlalchemy import Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy import Index
+from sqlalchemy import text
+
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .vehicle import Vehicle
+    from .insurance_policy import InsurancePolicy
+
+
+class Contract(Base):
+    __tablename__ = "contracts"
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    vehicle_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("vehicles.id"), nullable=False
+    )
+    total_price: Mapped[float] = mapped_column(Float, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now(timezone.utc), nullable=False
+    )
+    expired_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc) + timedelta(days=365),
+        nullable=False,
+    )
+    is_active: Mapped[Boolean] = mapped_column(Boolean, default=True, nullable=False)
+
+    vehicle: Mapped["Vehicle"] = relationship("Vehicle", back_populates="contracts")
+    policies: Mapped["InsurancePolicy"] = relationship(
+        "InsurancePolicy", secondary="contract_policies", back_populates="contracts"
+    )
+
+    __table_args__ = (
+        Index(
+            "uq_active_constraint_for_vehicle",
+            "vehicle_id",
+            unique=True,
+            postgresql_where=is_active,
+        ),
+    )
