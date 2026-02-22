02843dfe6e19fc90359342f6330c7e6e8ce9c60f
Cosimo Menga <cosimo_menga@ik.me>
2026-02-22 16:47:39 +0100

    Remove "Value error, " prefix from validation messages

--- backend/services/vehicle-police/app/main.py
+++ backend/services/vehicle-police/app/main.py
@@ -98,7 +98,7 @@ async def validation_exception_handler(request: Request,exc: RequestValidationEr
             "errors": [
                 {
                     "field": err["loc"][-1],
-                    "message": err["msg"],
+                    "message": err["msg"].replace("Value error, ",""),
                 }
                 for err in exc.errors()
             ],
