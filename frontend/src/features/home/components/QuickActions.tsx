6c5035293135a8e0f671c8dcc5239d3959abd778
cmenga <cosimo_menga@ik.me>
2026-02-25 10:57:53 +0100

    Rename modal ref to modalRef

--- frontend/src/features/home/components/QuickActions.tsx
+++ frontend/src/features/home/components/QuickActions.tsx
@@ -4,23 +4,23 @@ import FormContract from "./policies/FormContract";
 import { useRef } from "react";
 
 export default function QuickActions() {
-  const modal = useRef<HTMLDialogElement | null>(null);
+  const modalRef = useRef<HTMLDialogElement | null>(null);
 
   function handleOpen() {
-    const current = modal.current;
+    const current = modalRef.current;
     if (current) current.showModal();
   }
 
   function handleClose() {
-    const current = modal.current;
+    const current = modalRef.current;
     if (current) current.close(); 
   }
 
   return (
     <div className="bg-white rounded-2xl shadow-md p-6">
       <h3 className="text-xl font-bold text-gray-900 mb-4">Azioni Rapide</h3>
-      <div onClick={handleOpen} className="grid gap-4">
-        <button className="cursor-pointer flex items-center gap-3 p-4 border-2 border-gray-200 rounded-xl hover:border-blue-500 hover:bg-blue-50 transition">
+      <div  className="grid gap-4">
+        <button onClick={handleOpen} className="cursor-pointer flex items-center gap-3 p-4 border-2 border-gray-200 rounded-xl hover:border-blue-500 hover:bg-blue-50 transition">
           <div className="bg-blue-100 p-3 rounded-lg">
             <Plus className="w-6 h-6 text-blue-600" />
           </div>
@@ -30,7 +30,7 @@ export default function QuickActions() {
           </div>
         </button>
 
-        <Modal ref={modal}>
+        <Modal ref={modalRef}>
           <FormContract onClose={handleClose} />
         </Modal>
       </div>
